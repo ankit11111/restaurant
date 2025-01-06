8074b70fe897ff71eccd9be996c4e805d3b048cc
ankit11111 <ankit_agarwal_2@yahoo.com>
2025-01-06 16:02:28 +0530

    Update RestaurantServiceImpl.java

--- src/main/java/com/review/restaurant/service/impl/RestaurantServiceImpl.java
+++ src/main/java/com/review/restaurant/service/impl/RestaurantServiceImpl.java
@@ -1,14 +1,20 @@
 package com.review.restaurant.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import com.review.restaurant.converter.RestaurantConverter;
 import com.review.restaurant.dto.RestaurantDto;
 import com.review.restaurant.entity.Restaurant;
+import com.review.restaurant.entity.RestaurantReview;
 import com.review.restaurant.repository.RestaurantRepository;
+import com.review.restaurant.repository.RestaurantReviewRepository;
 import com.review.restaurant.service.RestaurantService;
 
 @Service
@@ -16,6 +22,9 @@ public class RestaurantServiceImpl implements RestaurantService {
 
 	@Autowired
 	private RestaurantRepository restaurantRepository;
+
+	@Autowired
+	private RestaurantReviewRepository restauranReviewRepository;
 
 	@Override
 	public Long createRestaurant(RestaurantDto restaurantDto) {
@@ -29,6 +38,32 @@ public class RestaurantServiceImpl implements RestaurantService {
 			return RestaurantConverter.toRestaurantDto(restaurant.get());
 		}
 		return null;
+	}
+
+	@Override
+	public List<RestaurantDto> listAllRestaurants(Double minimumRating) {
+		List<Restaurant> restaurantList = null;
+		List<RestaurantDto> restaurantDtoList = new ArrayList<RestaurantDto>();
+		restaurantList = restaurantRepository.findAll();
+		if (Objects.nonNull(minimumRating)) {
+			restaurantList.stream().forEach(restaurant -> {
+				Double sum = 0.0;
+				for (RestaurantReview review : restaurant.getReviews()) {
+					sum += review.getRating();
+				}
+				Double averageRating = sum / restaurant.getReviews().size();
+				if (averageRating >= minimumRating) {
+					restaurantDtoList.add(RestaurantConverter.toRestaurantDto(restaurant));
+				}
+
+			});
+		} else {
+			restaurantList.stream().forEach(restaurant -> {
+				restaurantDtoList.add(RestaurantConverter.toRestaurantDto(restaurant));
+
+			});
+		}
+		return restaurantDtoList;
 	}
 
 }
