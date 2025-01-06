a915d60d987951f7515c29cc1f5cbce7825627fb
ankit11111 <ankit_agarwal_2@yahoo.com>
2025-01-06 16:02:45 +0530

    Update RestaurantService.java

--- src/main/java/com/review/restaurant/service/RestaurantService.java
+++ src/main/java/com/review/restaurant/service/RestaurantService.java
@@ -1,5 +1,7 @@
 package com.review.restaurant.service;
 
+import java.util.List;
+
 import com.review.restaurant.dto.RestaurantDto;
 
 public interface RestaurantService {
@@ -7,4 +9,6 @@ public interface RestaurantService {
 	Long createRestaurant(final RestaurantDto restaurantDto);
 
 	RestaurantDto getRestaurantById(final Long restaurantId);
+
+	List<RestaurantDto> listAllRestaurants(final Double minimumRating);
 }
