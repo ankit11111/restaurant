afde1b3274b423a6913c4e0b9bd02afd4c964750
ankit11111 <ankit_agarwal_2@yahoo.com>
2025-01-06 16:02:59 +0530

    Update PathConstants.java

--- src/main/java/com/review/restaurant/constants/PathConstants.java
+++ src/main/java/com/review/restaurant/constants/PathConstants.java
@@ -7,6 +7,7 @@ public final class PathConstants {
 	}
 
 	public static final String CREATE_RESTAURANT_PATH = "/restaurant";
+	public static final String GET_ALL_RESTAURANTS = "/restaurants";
 	public static final String GET_RESTAURANT_PATH = CREATE_RESTAURANT_PATH + "/{restaurantId}";
 	public static final String CREATE_RESTAURANT_REVIEW = GET_RESTAURANT_PATH + "/review";
 	public static final String GET_RESTAURANT_REVIEWS = GET_RESTAURANT_PATH + "/reviews";
